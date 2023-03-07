566840dc0fb95677652f3b1cec9b7612f9157e39
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-03-07 15:08:09 +0200

    update contactsReduser

--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -8,14 +8,40 @@ const contactsReducer = createSlice({
     isLoading: false,
     error: null,
   },
+  reducers: {
+    AddContact: {
+      reducer(state, action) {
+        console.log(state.entities);
+        state.entities.push(action.payload);
+      },
+      prepare(entities) {
+        return {
+          payload: entities,
+        };
+      },
+    },
+    DeleteContact(state, action) {
+      const index = state.entities.findIndex(
+        userId => userId.id === action.payload
+      );
+      state.entities.splice(index, 1);
+    },
+
+    SearchByName: (state, action) => {
+      state.filter = action.payload.toLowerCase();
+    },
+  },
   extraReducers: {
-    [fetchContacts.fulfilled]: (state, action) => {
-      state.entities = action.payload;
+    [fetchContacts.fulfilled]: (state, { payload }) => {
+      state.entities = payload;
     },
     [fetchContacts.pending]: state => {
       state.isLoading = true;
     },
   },
 });
+
+export const { AddContact, DeleteContact, SearchByName } =
+  contactsReducer.actions;
 
 export default contactsReducer.reducer;
